196ae656ee0d660bbd337292b5161e059262f7ca
Aida Monfort Muriach <aida.monfort@uji.es>
2023-04-20 15:07:19 +0200

    visualizeBoK returns promise, improve search highlihgting

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ let codesColors = [];
 
 let selectedNodes = [];
 let allNodes = [];
+let rootNodeCode = '';
 let fullBoK = {};
+
+const COLOR_STROKE_SELECTED = 'black';
+const COLOR_STROKE_DEFAULT = '#090909';
+const COLOR_STROKE_RESULTS = '#080808';
 
 export function parseBOKData(bokJSON) {
   // loop all nodes
@@ -100,7 +105,8 @@ export function parseBOKData(bokJSON) {
   for (let i = 0; i < allNodes.length; i++) {
     if (allNodes[i].parents.length == 0) {
       rootNode = allNodes[i];
-      console.log("ROOT NODE " + i)
+      rootNodeCode = allNodes[i].code.toLowerCase();
+      console.log("ROOT NODE " + i + " code " + rootNodeCode)
       break;
     }
   }
@@ -125,169 +131,172 @@ export function parseBOKData(bokJSON) {
 
 
 export function browseToConcept(code) {
-
-  var node = d3.select('#node-' + code).data();
-  zoom(node[0]);
-  displayConcept(node[0]);
+  var node = d3.select('#node-' + code.toLowerCase()).data();
+  // Can not find the node, display root
+  if (node.length == 0) {
+    node = d3.select('#node-' + rootNodeCode.toLowerCase()).data();
+    zoom(node[0]);
+    displayError();
+  } else {
+    zoom(node[0]);
+    displayConcept(node[0]);
+  }
 }
 
 window.browseToConcept = browseToConcept;
 
 export function visualizeBOKData(url) {
 
-  d3.json(url + '.json ').then((bok, error) => {
-    // Parse current version of BoK
-    var bokData = parseBOKData(bok.current);
-    if (error) throw error;
+  return new Promise(resolve => {
 
-    const COLOR_STROKE_SELECTED = 'grey';
+    d3.json(url + '.json ').then((bok, error) => {
+      // Parse current version of BoK
+      var bokData = parseBOKData(bok.current);
+      if (error) throw error;
 
-    var pack = data => d3.pack()
-      .size([width, height])
-      .padding(3)
-      (d3.hierarchy(bokData)
-        .sum(d => d.value)
-        .sort((a, b) => b.value - a.value));
+      var pack = data => d3.pack()
+        .size([width, height])
+        .padding(3)
+        (d3.hierarchy(bokData)
+          .sum(d => d.value)
+          .sort((a, b) => b.value - a.value));
 
-    /*     var color = d3.scaleLinear()
-          .domain([0, 5])
-          .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-          .interpolate(d3.interpolateHcl); */
+      /*     var color = d3.scaleLinear()
+            .domain([0, 5])
+            .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+            .interpolate(d3.interpolateHcl); */
 
-    // var color = d3.interpolateRainbow();
+      // var color = d3.interpolateRainbow();
 
-    //  ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+      //  ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 
-    var root = pack(bokData);
+      var root = pack(bokData);
 
-    let focus = root;
+      let focus = root;
 
-    const svg = d3.select('#' + TEMPLATE_IdGraph)
-      .append("svg")
-      .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-      .style("display", "block")
-      // .style("margin", "0 -14px")
-      //  .style("background", d3.interpolateRainbow(0))
-      .style("cursor", "pointer")
-      .on("click", () => zoom(root));
+      const svg = d3.select('#' + TEMPLATE_IdGraph)
+        .append("svg")
+        .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+        .style("display", "block")
+        .style("cursor", "pointer")
+        .on("click", () => zoom(root));
 
-    const node = svg.append("g")
-      .selectAll("circle")
-      .data(root)
-      // .data(root.descendants().slice(1))  // If we need to remove first element, in case root is duplicated
-      .join("circle")
-      // .attr("fill", d => d.children ? color(d.depth / 10) : "white")
-      .attr("fill", d => {
-        // TODO find color in array no more than coloScheme array number
-        let code = codesColors.indexOf(d.data.code.substring(0, 2));
-        while (code >= d3.schemeSet3.length)
-          code = code - d3.schemeSet3.length;
+      const node = svg.append("g")
+        .selectAll("circle")
+        .data(root)
+        // .data(root.descendants().slice(1))  // If we need to remove first element, in case root is duplicated
+        .join("circle")
+        .attr("fill", d => {
+          // TODO find color in array no more than coloScheme array number
+          let code = codesColors.indexOf(d.data.code.substring(0, 2));
+          while (code >= d3.schemeSet3.length)
+            code = code - d3.schemeSet3.length;
 
-        return d3.schemeSet3[code];
-      })
-      //.attr("fill", d => d3.schemeSet3[d.depth])
-      .attr("stroke", "black")
-      .attr("stroke-width", "0.2px")
-      .attr("id", d => "node-" + d.data.code)
-      // .attr("pointer-events", d => !d.children ? "none" : null)
-      .on("click", (event, d) => {
-        if (focus !== d) {
-          event.currentTarget.style.stroke = COLOR_STROKE_SELECTED;
-          zoom(d);
-          displayConcept(d);
-        }
-        event.stopPropagation();
-      })
-      .on("mouseover", function (d) {
-        if (this.style.stroke != COLOR_STROKE_SELECTED) this.style.strokeWidth = 1
-      })
-      .on("mouseleave", function (d) {
-        if (this.style.stroke != COLOR_STROKE_SELECTED) this.style.strokeWidth = 0.2
-      });;
-
-    const label = svg.append("g")
-      .style("font", "12px sans-serif")
-      .attr("pointer-events", "none")
-      .attr("text-anchor", "middle")
-      .selectAll("text")
-      .data(root.descendants())
-      .join("text")
-      .style("fill-opacity", d => d.parent === root ? 1 : 0)
-      .style("display", d => d.parent === root ? "inline" : "none")
-      .each(function (d) { //This function inserts a label and adds linebreaks, avoiding lines > 13 characters
-        var arr = d.data.name.split(' '),
-          maxLabelLength = 13,
-          final = [arr[0]];
-        for (var i = 1, j = 0; i < arr.length; i++) {
-          (final[j].length + arr[i].length < maxLabelLength) ? final[j] += ' ' + arr[i]: (j++, final[j] = arr[i]);
-        }
-        final.forEach((t, i) => d3.select(this).append('tspan').text(t).attr('dy', i ? '1em' : -0.5 * (j - 1) + 'em').attr('x', 0).attr('text-anchor', 'middle').attr('class', 'tspan' + i));
-      })
-
-    zoomTo([root.x, root.y, root.r * 2]);
-
-    function zoomTo(v) {
-      const k = width / v[2];
-      view = v;
-
-      label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-      node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-      node.attr("r", d => d.r * k);
-    }
-
-    function zoom(d) {
-      const focus0 = focus;
-
-      focus = d;
-
-      const transition = svg.transition()
-        .duration(750)
-        .tween("zoom", d => {
-          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-          return t => zoomTo(i(t));
+          return d3.schemeSet3[code];
+        })
+        .attr("stroke", COLOR_STROKE_DEFAULT)
+        .attr("stroke-width", "0.2px")
+        .attr("id", d => "node-" + d.data.code.toLowerCase())
+        .on("click", (event, d) => {
+          if (focus !== d) {
+            event.currentTarget.style.stroke = COLOR_STROKE_SELECTED;
+            zoom(d);
+            displayConcept(d);
+          }
+          event.stopPropagation();
+        }).on("mouseover", function (d) {
+          if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 1
+        })
+        .on("mouseleave", function (d) {
+          if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 0.2
         });
 
-      label
-        .filter(function (d) {
-          return d.parent === focus || this.style.display === "inline" || (d === focus && (d.children == null || d.children == []));
+      const label = svg.append("g")
+        .style("font", "12px sans-serif")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(root.descendants())
+        .join("text")
+        .style("fill-opacity", d => d.parent === root ? 1 : 0)
+        .style("display", d => d.parent === root ? "inline" : "none")
+        .each(function (d) { //This function inserts a label and adds linebreaks, avoiding lines > 13 characters
+          var arr = d.data.name.split(' '),
+            maxLabelLength = 13,
+            final = [arr[0]];
+          for (var i = 1, j = 0; i < arr.length; i++) {
+            (final[j].length + arr[i].length < maxLabelLength) ? final[j] += ' ' + arr[i]: (j++, final[j] = arr[i]);
+          }
+          final.forEach((t, i) => d3.select(this).append('tspan').text(t).attr('dy', i ? '1em' : -0.5 * (j - 1) + 'em').attr('x', 0).attr('text-anchor', 'middle').attr('class', 'tspan' + i));
         })
-        .transition(transition)
-        .style("fill-opacity", function (d) {
-          return d.parent === focus || (d === focus && (d.children == null || d.children == [])) ? 1 : 0;
-        })
-        .on("start", function (d) {
-          if (d.parent === focus || (d === focus && (d.children == null || d.children == [])))
-            this.style.display = "inline";
-        })
-        .on("end", function (d) {
-          if (d.parent !== focus && (d !== focus && (d.children == null || d.children == [])))
-            this.style.display = "none";
-        });
 
-    }
+      zoomTo([root.x, root.y, root.r * 2]);
 
-    window.zoom = zoom;
-    displayConcept(root);
+      function zoomTo(v) {
+        const k = width / v[2];
+        view = v;
 
+        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("r", d => d.r * k);
+      }
+
+      function zoom(d) {
+        const focus0 = focus;
+
+        focus = d;
+
+        const transition = svg.transition()
+          .duration(750)
+          .tween("zoom", d => {
+            const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+            return t => zoomTo(i(t));
+          });
+
+        label
+          .filter(function (d) {
+            return d.parent === focus || this.style.display === "inline" || (d === focus && (d.children == null || d.children == []));
+          })
+          .transition(transition)
+          .style("fill-opacity", function (d) {
+            return d.parent === focus || (d === focus && (d.children == null || d.children == [])) ? 1 : 0;
+          })
+          .on("start", function (d) {
+            if (d.parent === focus || (d === focus && (d.children == null || d.children == [])))
+              this.style.display = "inline";
+          })
+          .on("end", function (d) {
+            if (d.parent !== focus && (d !== focus && (d.children == null || d.children == [])))
+              this.style.display = "none";
+          });
+
+      }
+
+      window.zoom = zoom;
+      displayConcept(root);
+      resolve('BoK Loaded - resolve');
+
+    });
   });
 
 }
 
 export function searchInBoK(string, searchCode, searchName, searchDes, searchSkills) {
   cleanSearchInBOK();
+  cleanTextInfo();
 
-  let searchInputFieldDoc = string.trim();
+  let searchInputFieldDoc = string.trim().toLowerCase();
   if (searchInputFieldDoc != "" && searchInputFieldDoc != " ") {
 
     let results = allNodes.filter((n) => {
-      let filterBool = searchCode && n.code.includes(searchInputFieldDoc) ||
-        searchName && n.name.includes(searchInputFieldDoc) ||
-        searchDes && n.description.includes(searchInputFieldDoc);
+      let filterBool = searchCode && n.code.toLowerCase().includes(searchInputFieldDoc) ||
+        searchName && n.name.toLowerCase().includes(searchInputFieldDoc) ||
+        searchDes && n.description.toLowerCase().includes(searchInputFieldDoc);
 
       // search for coincidences in demonstrableSkills
       if (searchSkills && !filterBool) {
         n.demonstrableSkills.forEach(s => {
-          if (s.includes(searchInputFieldDoc)) {
+          if (s.toLowerCase().includes(searchInputFieldDoc)) {
             filterBool = true;
           }
         });
@@ -297,62 +306,44 @@ export function searchInBoK(string, searchCode, searchName, searchDes, searchSki
     });
 
     results.forEach(n => {
-      d3.select('#node-' + n.code)
-        .attr("stroke-width", "2px");
+      d3.select('#node-' + n.code.toLowerCase())
+        .attr("stroke-width", "2px")
+        .attr("stroke", COLOR_STROKE_RESULTS);
     });
 
     selectedNodes = results;
 
     return selectedNodes;
+  } else {
+    browseToConcept('BOK');
+    // navigateToRoot();
+    return [];
   }
 }
 
 export function cleanSearchInBOK() {
   //clean search
   selectedNodes.forEach(n => {
-    d3.select('#node-' + n.code)
-      .attr("stroke-width", "0.2px");
+    d3.select('#node-' + n.code.toLowerCase())
+      .attr("stroke-width", "0.2px")
+      .attr("stroke", COLOR_STROKE_DEFAULT);
   });
   selectedNodes = [];
 }
 
-
-
-/* export function zoomTo(v) {
-  const k = width / v[2];
-  view = v;
-
-  label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-  node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-  node.attr("r", d => d.r * k);
+export function navigateToRoot() {
+  const root = d3.select('#node-BOK');
+  displayConcept(root.data()[0]);
 }
 
-export function zoom(event, d) {
-  const focus0 = focus;
+export function cleanTextInfo() {
+  var mainNode = document.getElementById(TEMPLATE_IdText)
+  mainNode.innerHTML = "";
+}
 
-  focus = d;
-
-  const transition = svg.transition()
-    .duration(event.altKey ? 7500 : 750)
-    .tween("zoom", d => {
-      const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-      return t => zoomTo(i(t));
-    });
-
-  label
-    .filter(function (d) {
-      return d.parent === focus || this.style.display === "inline";
-    })
-    .transition(transition)
-    .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-    .on("start", function (d) {
-      if (d.parent === focus) this.style.display = "inline";
-    })
-    .on("end", function (d) {
-      if (d.parent !== focus) this.style.display = "none";
-    });
-} */
-
+export function displayError() {
+  console.log("Concept does not exists")
+}
 
 //displays all available content for the currently focussed concept in the description box:
 export function displayConcept(d) {
@@ -362,15 +353,13 @@ export function displayConcept(d) {
 
   var titleNode = document.createElement("h1");
   titleNode.id = "boktitle";
-  // titleNode.attributes = "#boktitle";
   titleNode.innerHTML = "[" + d.data.code + "] " + d.data.name; //display Name and shortcode of concept:
-  // titleNode.style = "margin-bottom: 0px;";
 
-  //   window.history.pushState({}, "Find In Bok", "/" + d.data.code);
+  // window.history.pushState({}, "Find In Bok", "/" + d.data.code);
 
-  // var pNode = document.createElement("p");
-  //  pNode.innerHTML = "Permalink: <a href= 'https://bok.eo4geo.eu/" + d.code + "'> https://bok.eo4geo.eu/" + d.code + "</a>";
-  //  mainNode.appendChild(pNode);
+  var pNode = document.createElement("p");
+  pNode.innerHTML = "Permalink: <a href= 'https://ucgis-bok.web.app/" + d.data.code + "' target='blank'> https://ucgis-bok.web.app/" + d.data.code + "</a>";
+  mainNode.appendChild(pNode);
 
   mainNode.appendChild(titleNode);
   if (d.data.selfAssesment != " ") {
@@ -452,29 +441,3 @@ export function displayTextList(array, domElement, headline) {
   text += "</ul></div>";
   domElement.innerHTML += text;
 };
-
-/* export function browseToConcept(code) {
-
-  var node = d3.select('#sc-' + code);
-  displayConcept(node);
-
-var node = null;
-   codSelected = code;
-   dataAndFunctions.nodes.forEach(n => {
-     if (n.data.code == code) {
-       node = n;
-       return;
-     }
-   });
-   if (node != null) {
-     var nodeData = dataAndFunctions.conceptNodeCollection.getNodeByNameShort(code);
-     displayConcept(nodeData);
-     dataAndFunctions.zoom(node);
-   }
-   if (isAnOldVersion) {
-     isAnObsoleteId = true;
-     isAnOldVersion = false;
-   
-
-     
-} */
