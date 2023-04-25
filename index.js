8399670ad94c9cee61641229e4feac9573183375
Aida Monfort Muriach <aida.monfort@uji.es>
2023-04-25 13:41:05 +0200

    navigate between versions update graph, show error msg

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ let allNodes = [];
 let rootNodeCode = '';
 let fullBoK = {};
 
+let allVersions = [];
+let currSelCode = '';
+
 const COLOR_STROKE_SELECTED = 'black';
 const COLOR_STROKE_DEFAULT = '#090909';
 const COLOR_STROKE_RESULTS = '#080808';
@@ -131,12 +134,14 @@ export function parseBOKData(bokJSON) {
 
 
 export function browseToConcept(code) {
+  currSelCode = code.toLowerCase();
   var node = d3.select('#node-' + code.toLowerCase()).data();
   // Can not find the node, display root
   if (node.length == 0) {
     node = d3.select('#node-' + rootNodeCode.toLowerCase()).data();
     zoom(node[0]);
-    displayError();
+    displayConcept(node[0]);
+    displayError(code);
   } else {
     zoom(node[0]);
     displayConcept(node[0]);
@@ -145,139 +150,163 @@ export function browseToConcept(code) {
 
 window.browseToConcept = browseToConcept;
 
-export function visualizeBOKData(url) {
-
+export function getBoKData(url) {
   return new Promise(resolve => {
-
     d3.json(url + '.json ').then((bok, error) => {
-      // Parse current version of BoK
-      var bokData = parseBOKData(bok.current);
-      if (error) throw error;
-
-      var pack = data => d3.pack()
-        .size([width, height])
-        .padding(3)
-        (d3.hierarchy(bokData)
-          .sum(d => d.value)
-          .sort((a, b) => b.value - a.value));
-
-      /*     var color = d3.scaleLinear()
-            .domain([0, 5])
-            .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-            .interpolate(d3.interpolateHcl); */
-
-      // var color = d3.interpolateRainbow();
-
-      //  ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-
-      var root = pack(bokData);
-
-      let focus = root;
-
-      const svg = d3.select('#' + TEMPLATE_IdGraph)
-        .append("svg")
-        .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-        .style("display", "block")
-        .style("cursor", "pointer")
-        .on("click", () => zoom(root));
-
-      const node = svg.append("g")
-        .selectAll("circle")
-        .data(root)
-        // .data(root.descendants().slice(1))  // If we need to remove first element, in case root is duplicated
-        .join("circle")
-        .attr("fill", d => {
-          // TODO find color in array no more than coloScheme array number
-          let code = codesColors.indexOf(d.data.code.substring(0, 2));
-          while (code >= d3.schemeSet3.length)
-            code = code - d3.schemeSet3.length;
-
-          return d3.schemeSet3[code];
-        })
-        .attr("stroke", COLOR_STROKE_DEFAULT)
-        .attr("stroke-width", "0.2px")
-        .attr("id", d => "node-" + d.data.code.toLowerCase())
-        .on("click", (event, d) => {
-          if (focus !== d) {
-            event.currentTarget.style.stroke = COLOR_STROKE_SELECTED;
-            zoom(d);
-            displayConcept(d);
-          }
-          event.stopPropagation();
-        }).on("mouseover", function (d) {
-          if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 1
-        })
-        .on("mouseleave", function (d) {
-          if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 0.2
-        });
-
-      const label = svg.append("g")
-        .style("font", "12px sans-serif")
-        .attr("pointer-events", "none")
-        .attr("text-anchor", "middle")
-        .selectAll("text")
-        .data(root.descendants())
-        .join("text")
-        .style("fill-opacity", d => d.parent === root ? 1 : 0)
-        .style("display", d => d.parent === root ? "inline" : "none")
-        .each(function (d) { //This function inserts a label and adds linebreaks, avoiding lines > 13 characters
-          var arr = d.data.name.split(' '),
-            maxLabelLength = 13,
-            final = [arr[0]];
-          for (var i = 1, j = 0; i < arr.length; i++) {
-            (final[j].length + arr[i].length < maxLabelLength) ? final[j] += ' ' + arr[i]: (j++, final[j] = arr[i]);
-          }
-          final.forEach((t, i) => d3.select(this).append('tspan').text(t).attr('dy', i ? '1em' : -0.5 * (j - 1) + 'em').attr('x', 0).attr('text-anchor', 'middle').attr('class', 'tspan' + i));
-        })
-
-      zoomTo([root.x, root.y, root.r * 2]);
-
-      function zoomTo(v) {
-        const k = width / v[2];
-        view = v;
-
-        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-        node.attr("r", d => d.r * k);
-      }
-
-      function zoom(d) {
-        const focus0 = focus;
-
-        focus = d;
-
-        const transition = svg.transition()
-          .duration(750)
-          .tween("zoom", d => {
-            const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-            return t => zoomTo(i(t));
-          });
-
-        label
-          .filter(function (d) {
-            return d.parent === focus || this.style.display === "inline" || (d === focus && (d.children == null || d.children == []));
-          })
-          .transition(transition)
-          .style("fill-opacity", function (d) {
-            return d.parent === focus || (d === focus && (d.children == null || d.children == [])) ? 1 : 0;
-          })
-          .on("start", function (d) {
-            if (d.parent === focus || (d === focus && (d.children == null || d.children == [])))
-              this.style.display = "inline";
-          })
-          .on("end", function (d) {
-            if (d.parent !== focus && (d !== focus && (d.children == null || d.children == [])))
-              this.style.display = "none";
-          });
-
-      }
-
-      window.zoom = zoom;
-      displayConcept(root);
+      fullBoK = bok;
       resolve('BoK Loaded - resolve');
-
+      if (error) throw error;
     });
   });
+}
+
+export function visualizeBoKVersion(version) {
+
+  // Parse 'current' version of BoK
+  var bokData = parseBOKData(fullBoK[version]);
+
+  var pack = data => d3.pack()
+    .size([width, height])
+    .padding(3)
+    (d3.hierarchy(bokData)
+      .sum(d => d.value)
+      .sort((a, b) => b.value - a.value));
+
+  /*     var color = d3.scaleLinear()
+        .domain([0, 5])
+        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+        .interpolate(d3.interpolateHcl); */
+
+  // var color = d3.interpolateRainbow();
+
+  //  ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+
+  var root = pack(bokData);
+
+  let focus = root;
+
+  d3.select('#' + TEMPLATE_IdGraph)
+    .select("svg").selectAll("circle").remove();
+
+  const svg = d3.select('#' + TEMPLATE_IdGraph)
+    .select("svg")
+    .on("click", () => zoom(root));
+
+  const node = svg.append("g")
+    .selectAll("circle")
+    .data(root)
+    // .data(root.descendants().slice(1))  // If we need to remove first element, in case root is duplicated
+    .join("circle")
+    .attr("fill", d => {
+      // TODO find color in array no more than coloScheme array number
+      let code = codesColors.indexOf(d.data.code.substring(0, 2));
+      while (code >= d3.schemeSet3.length)
+        code = code - d3.schemeSet3.length;
+
+      return d3.schemeSet3[code];
+    })
+    .attr("stroke", COLOR_STROKE_DEFAULT)
+    .attr("stroke-width", "0.2px")
+    .attr("id", d => "node-" + d.data.code.toLowerCase())
+    .on("click", (event, d) => {
+      if (focus !== d) {
+        event.currentTarget.style.stroke = COLOR_STROKE_SELECTED;
+        zoom(d);
+        displayConcept(d);
+      }
+      event.stopPropagation();
+    }).on("mouseover", function (d) {
+      if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 1
+    })
+    .on("mouseleave", function (d) {
+      if (this.attributes.stroke.value === COLOR_STROKE_DEFAULT) this.style.strokeWidth = 0.2
+    });
+
+  const label = svg.append("g")
+    .style("font", "12px sans-serif")
+    .attr("pointer-events", "none")
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+    .style("fill-opacity", d => d.parent === root ? 1 : 0)
+    .style("display", d => d.parent === root ? "inline" : "none")
+    .each(function (d) { //This function inserts a label and adds linebreaks, avoiding lines > 13 characters
+      var arr = d.data.name.split(' '),
+        maxLabelLength = 13,
+        final = [arr[0]];
+      for (var i = 1, j = 0; i < arr.length; i++) {
+        (final[j].length + arr[i].length < maxLabelLength) ? final[j] += ' ' + arr[i]: (j++, final[j] = arr[i]);
+      }
+      final.forEach((t, i) => d3.select(this).append('tspan').text(t).attr('dy', i ? '1em' : -0.5 * (j - 1) + 'em').attr('x', 0).attr('text-anchor', 'middle').attr('class', 'tspan' + i));
+    })
+
+  zoomTo([root.x, root.y, root.r * 2]);
+
+  function zoomTo(v) {
+    const k = width / v[2];
+    view = v;
+
+    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("r", d => d.r * k);
+  }
+
+  function zoom(d) {
+    const focus0 = focus;
+
+    focus = d;
+
+    const transition = svg.transition()
+      .duration(750)
+      .tween("zoom", d => {
+        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+        return t => zoomTo(i(t));
+      });
+
+    label
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline" || (d === focus && (d.children == null || d.children == []));
+      })
+      .transition(transition)
+      .style("fill-opacity", function (d) {
+        return d.parent === focus || (d === focus && (d.children == null || d.children == [])) ? 1 : 0;
+      })
+      .on("start", function (d) {
+        if (d.parent === focus || (d === focus && (d.children == null || d.children == [])))
+          this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus && (d !== focus && (d.children == null || d.children == [])))
+          this.style.display = "none";
+      });
+
+  }
+
+  window.zoom = zoom;
+
+
+
+  browseToConcept(currSelCode);
+
+  // displayConcept(root);
+
+}
+
+window.visualizeBoKVersion = visualizeBoKVersion;
+
+
+export async function visualizeBOKData(url, version) {
+
+  await getBoKData(url);
+  allVersions = Object.keys(fullBoK);
+  d3.select('#' + TEMPLATE_IdGraph)
+    .append("svg")
+    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+    .style("display", "block")
+    .style("cursor", "pointer");
+
+  visualizeBoKVersion(version);
 
 }
 
@@ -341,8 +370,10 @@ export function cleanTextInfo() {
   mainNode.innerHTML = "";
 }
 
-export function displayError() {
+export function displayError(code) {
   console.log("Concept does not exists")
+  var mainNode = document.getElementById(TEMPLATE_IdText);
+  mainNode.innerHTML = "<p style='color:#c60606;'> Concept " + code + " does not exist. Use the links or the graph to navigate to a valid one. </p> " + mainNode.innerHTML;
 }
 
 //displays all available content for the currently focussed concept in the description box:
@@ -355,7 +386,7 @@ export function displayConcept(d) {
   titleNode.id = "boktitle";
   titleNode.innerHTML = "[" + d.data.code + "] " + d.data.name; //display Name and shortcode of concept:
 
-  // window.history.pushState({}, "Find In Bok", "/" + d.data.code);
+  window.history.pushState({}, "Find In Bok", "/" + d.data.code);
 
   var pNode = document.createElement("p");
   pNode.innerHTML = "Permalink: <a href= 'https://ucgis-bok.web.app/" + d.data.code + "' target='blank'> https://ucgis-bok.web.app/" + d.data.code + "</a>";
@@ -401,7 +432,8 @@ export function displayConcept(d) {
   d.data.contributors && d.data.contributors.length > 0 ? displayLinksList(d.data.contributors, infoNode, "Contributors") : null;
   d.data.sourceDocuments && d.data.sourceDocuments.length > 0 ? displayLinksList(d.data.sourceDocuments, infoNode, "Source Documents") : null;
 
-  //  displayVersions(d.nameShort, infoNode, numVersion, yearVersion);
+  displayVersions(infoNode);
+  // displayVersions(d.nameShort, infoNode, numVersion, yearVersion);
 
   mainNode.appendChild(infoNode);
 
@@ -439,6 +471,19 @@ export function displayTextList(array, domElement, headline) {
   var text = "<h2>" + headline + " [" + array.length + "] </h2><div><ul>";
   array.forEach(l => {
     text += "<li>" + l + "</li>";
+  });
+
+  text += "</ul></div>";
+  domElement.innerHTML += text;
+};
+
+// displays list such as skills
+export function displayVersions(domElement) {
+
+  var text = "<h2> Versions [" + allVersions.length + "] </h2><div><ul>";
+  allVersions.forEach(v => {
+
+    text += `<li> <a style='color: #007bff; font-weight: 400; cursor: pointer;' onclick='visualizeBoKVersion(\"${v}\")' >${v}</a></li>`;
   });
 
   text += "</ul></div>";
